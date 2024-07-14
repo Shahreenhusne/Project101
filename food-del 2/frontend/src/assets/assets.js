dc819b4b7c252af5a32e0eab861dcb816628cd8c
shahreen <140973098+ShahreenHRabbani@users.noreply.github.com>
2024-07-15 04:30:13 +0600

    feat: frontend

--- food-del 2/frontend/src/assets/assets.js
+++ food-del 2/frontend/src/assets/assets.js
@@ -2,19 +2,19 @@ import basket_icon from './basket_icon.png'
 import logo from './logo.png'
 import header_img from './header_img.png'
 import search_icon from './search_icon.png'
-import menu_1 from './menu_1.png'
-import menu_2 from './menu_2.png'
-import menu_3 from './menu_3.png'
-import menu_4 from './menu_4.png'
-import menu_5 from './menu_5.png'
-import menu_6 from './menu_6.png'
-import menu_7 from './menu_7.png'
-import menu_8 from './menu_8.png'
+import menu_1 from './Burger Pictures/4.jpg'
+import menu_2 from './Biryani/1.jpg'
+import menu_3 from './Chowmein/1.jpg'
+import menu_4 from './French Fries/4.jpg'
+import menu_5 from './Fried Chicken/4.jpg'
+import menu_6 from './Fried Rice/2.jpg'
+import menu_7 from './Pizza/6.jpg'
+import menu_8 from './Sandwich/2.jpg'
 
-import food_1 from './food_1.png'
-import food_2 from './food_2.png'
-import food_3 from './food_3.png'
-import food_4 from './food_4.png'
+import food_1 from './Burger Pictures/1.jpg'
+import food_2 from './Burger Pictures/2.jpg'
+import food_3 from './Burger Pictures/3.jpg'
+import food_4 from './Burger Pictures/5.jpg'
 import food_5 from './food_5.png'
 import food_6 from './food_6.png'
 import food_7 from './food_7.png'
@@ -88,68 +88,68 @@ export const assets = {
 
 export const menu_list = [
     {
-        menu_name: "Salad",
+        menu_name: "Burger",
         menu_image: menu_1
     },
     {
-        menu_name: "Rolls",
+        menu_name: "Biryani",
         menu_image: menu_2
     },
     {
-        menu_name: "Deserts",
+        menu_name: "Chowmein",
         menu_image: menu_3
     },
     {
-        menu_name: "Sandwich",
+        menu_name: "French Fries",
         menu_image: menu_4
     },
     {
-        menu_name: "Cake",
+        menu_name: "Fried Chicken",
         menu_image: menu_5
     },
     {
-        menu_name: "Pure Veg",
+        menu_name: "Fried Rice",
         menu_image: menu_6
     },
     {
-        menu_name: "Pasta",
+        menu_name: "Pizza",
         menu_image: menu_7
     },
     {
-        menu_name: "Noodles",
+        menu_name: "Sandwich",
         menu_image: menu_8
     }]
 
 export const food_list = [
     {
         _id: "1",
-        name: "Greek salad",
+        name: "Burger",
         image: food_1,
-        price: 12,
+        price: 120,
         description: "Food provides essential nutrients for overall health and well-being",
-        category: "Salad"
+        category: "Burger"
     },
     {
         _id: "2",
-        name: "Veg salad",
+        name: "Burger",
         image: food_2,
         price: 18,
         description: "Food provides essential nutrients for overall health and well-being",
-        category: "Salad"
+        category: "Burger"
     }, {
         _id: "3",
-        name: "Clover Salad",
+        name: "Burger",
         image: food_3,
         price: 16,
         description: "Food provides essential nutrients for overall health and well-being",
-        category: "Salad"
+        category: "Burger"
     }, {
         _id: "4",
-        name: "Chicken Salad",
+        name: "Burger",
         image: food_4,
         price: 24,
         description: "Food provides essential nutrients for overall health and well-being",
-        category: "Salad"
+        category: "Burger"
     }, {
         _id: "5",
         name: "Lasagna Rolls",
